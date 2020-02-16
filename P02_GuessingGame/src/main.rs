fd97a0a9759fb9b186f4eccc02558db5767359ff
Killerpug <etapia.clr@outlook.com>
2020-02-16 13:07:57 -0600

    block comments

--- P02_GuessingGame/src/main.rs
+++ P02_GuessingGame/src/main.rs
@@ -4,27 +4,31 @@ use std::cmp::Ordering; //comparison
 
 fn main() {
     println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);   //ger_range is inclusive on the lower bound but exclusive on the upper bound.
+    //ger_range is inclusive on the lower bound but exclusive on the upper bound.
+    let secret_number = rand::thread_rng().gen_range(1, 101);  
     println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
-
-        let mut guess = String::new();          //mutable variable that is currently bound to a new, empty instance of a String
-
-        io::stdin().read_line(&mut guess)       //Invoke io library and use read input method. Returns Result(ok, err) type
+        //mutable variable that is currently bound to a new, empty instance of a String
+        let mut guess = String::new();         
+        //Invoke io library and use read input method. Returns Result(ok, err) type
+        io::stdin().read_line(&mut guess)       
             .expect("Failed to read line");     //expect() should be called to avoid warnings
 
-        //parse along with u32 enable us to convert the String type -> u32
-        let guess: u32 = match guess.trim().parse()  { //converts the String into a real number. Allowing comparison with secret_number
-            Ok(num) => num,                             //guess uses shadowing.trim eliminates white spaces.
-            Err(_) => continue                        //match ensures that user entry is a number, _ means any
+        /* guess uses shadowing. parse along with u32 enable us to convert the String type -> u32 
+        converting the String into a real number and allowing comparison with secret_number.
+        Trim() eliminates white spaces. match ensures that user entry is a number, _ means any */
+        let guess: u32 = match guess.trim().parse()  { 
+            Ok(num) => num,                             
+            Err(_) => continue                        
         };
 
         println!("You guessed: {}", guess);
-         //match allows us to compare a value against a series of patterns and then execute code based on which pattern matches.
-        match guess.cmp(&secret_number) {           //cmp compares 2 values & returns an enum(Less, Greater, Equial)
+         /*match allows us to compare a value against a series of patterns and then 
+         execute code based on which pattern matches. 
+         cmp() compares 2 values & returns a Result enum(Less, Greater, Equial) */
+        match guess.cmp(&secret_number) {         
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
