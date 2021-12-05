2e7dbcf129660a72777ca6b7a1a960aea950f2d7
Killerpug <etapia.clr@outlook.com>
2021-12-04 22:57:55 -0600

    Day 01: sonar with functional closure for parsing
    Benchamark
    - Time elapsed in sonar is: 1.1232ms(traditional)
    - Time elapsed in sonar functional is: 704.5µs(iterators)
    - Time elapsed in sonar functional2 is: 645.2µs(closure for parsing )

--- Projects/01_AdventCode2021/advent_of_code/src/d01_sonar_sweep.rs
+++ Projects/01_AdventCode2021/advent_of_code/src/d01_sonar_sweep.rs
@@ -1,7 +1,9 @@
 // count the number of times depth measurement increases from previous measurement.
 
 pub mod sonar_sweep{
+    use std::fs;
     use std::fs::File;
+    use std::error::Error; 
     use std::i32::{MAX, MIN};
     use std::io::{prelude::*, BufReader};
 
@@ -38,7 +40,53 @@ pub mod sonar_sweep{
         Ok(())
     }
 
+    // depth sonar implemented with functional rust: Iterators 
+    pub fn depth_increase_functional() -> Result<(), Box<dyn Error + 'static>> {
+        let mut count_increased_depth = 0;
+        let mut previous_line:i32 = MAX;    // avoids wrongly increasing count on first value
+        
+        let reader = fs::read_to_string("src/input/01_sweep_report.txt")?;
+        for line in reader.lines(){
+            let next_line =  match line.trim_end().parse::<i32>() {
+                Ok(line) => line,
+                _ => {
+                    println!("type is not integer, skiping line");
+                    MIN
+                },
+            };
+            if next_line == MIN {   //skip this value
+                continue;
+            }
+            if previous_line < next_line { // increased depth
+                count_increased_depth += 1;
+            }
+            previous_line = next_line;
+        }
+        println!("the deepth increased: {} times", count_increased_depth);
+        Ok(())
+    }
 
+    // depth sonar implemented with functional rust: Iterators and closures
+    pub fn depth_increase_functional2() -> Result<(), Box<dyn Error + 'static>> {
+        let mut count_increased_depth = 0;
+        let mut previous_line:i32 = MAX;    // avoids wrongly increasing count on first value
+        
+        let reader = fs::read_to_string("src/input/01_sweep_report.txt")?;
+        let split = reader.lines()
+            .map(|s|s.parse::<i32>()
+            .unwrap_or(MIN));
+        for line in split{
+            if line == MIN {   //skip this value
+                continue;
+            }
+            if previous_line < line { // increased depth
+                count_increased_depth += 1;
+            }
+            previous_line = line;
+        }
+        println!("the deepth increased: {} times", count_increased_depth);
+        Ok(())
+    }
 
 }
 
--- Projects/01_AdventCode2021/advent_of_code/src/main.rs
+++ Projects/01_AdventCode2021/advent_of_code/src/main.rs
@@ -1,5 +1,29 @@
 use advent_of_code::d01_sonar_sweep;
-
+use std::time::{ Instant};
 fn main() {
-     println!("status sonar: {:?}", d01_sonar_sweep::sonar_sweep::depth_increase());
+    let mut start = Instant::now();
+    println!(
+        "status sonar: {:?}",
+        d01_sonar_sweep::sonar_sweep::depth_increase()
+    );
+    let mut duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
+
+    start = Instant::now();
+    println!(
+        "status sonar functional: {:?}",
+        d01_sonar_sweep::sonar_sweep::depth_increase_functional()
+    );
+    duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
+
+    start = Instant::now();
+    println!(
+        "status sonar functional2: {:?}",
+        d01_sonar_sweep::sonar_sweep::depth_increase_functional2()
+    );
+    duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
