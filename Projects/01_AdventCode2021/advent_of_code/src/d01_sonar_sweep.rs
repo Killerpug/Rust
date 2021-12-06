8ab197ccf5068c5fbf0172eea2a21c0348f9d3c3
Killerpug <etapia.clr@outlook.com>
2021-12-05 22:42:11 -0600

    Day 01 finished: window-slider filter
    add 3 sample slider to filter noise.
    Note: with slider the increasing depth counter got higher since samples are normally on average higher than previous slider average.

--- Projects/01_AdventCode2021/advent_of_code/src/d01_sonar_sweep.rs
+++ Projects/01_AdventCode2021/advent_of_code/src/d01_sonar_sweep.rs
@@ -6,6 +6,10 @@ pub mod sonar_sweep{
     use std::error::Error; 
     use std::i32::{MAX, MIN};
     use std::io::{prelude::*, BufReader};
+    use std::collections::HashMap;
+
+    const NUMBER_OF_MEASUREMENTS: usize = 3;
+    const MAX_MEASUREMENTS: u32 = 3;
 
     pub fn depth_increase() -> Result<(), &'static str> {
         let mut count_increased_depth = 0;
@@ -36,7 +40,7 @@ pub mod sonar_sweep{
             previous_line = next_line;
         }
 
-        println!("the deepth increased: {} times", count_increased_depth);
+        println!("the depth increased: {} times", count_increased_depth);
         Ok(())
     }
 
@@ -62,7 +66,7 @@ pub mod sonar_sweep{
             }
             previous_line = next_line;
         }
-        println!("the deepth increased: {} times", count_increased_depth);
+        println!("the depth increased: {} times", count_increased_depth);
         Ok(())
     }
 
@@ -84,10 +88,84 @@ pub mod sonar_sweep{
             }
             previous_line = line;
         }
-        println!("the deepth increased: {} times", count_increased_depth);
+        println!("the depth increased: {} times", count_increased_depth);
         Ok(())
     }
 
+    //functional2 depth sonar with 3-measurement slide filter
+    pub fn dept_increase_slide_filter() -> Result<(), Box<dyn Error + 'static>> {
+        let mut count_increased_depth = 0;
+        let mut previous_avg:i32 = MAX;    // avoids wrongly increasing count on first value
+        let mut filter_depth = SlideFilter::new();
+        let mut pos = 0;
+        let mut result;
+        let mut new_avg;
+
+        let reader = fs::read_to_string("src/input/01_sweep_report.txt")?;
+        let split = reader.lines()
+            .map(|s|s.parse::<i32>()
+            .unwrap_or(MIN));
+        for line in split{
+            if line == MIN {   //skip this value
+                continue;
+            }
+            result = filter_depth.calculate_avg(line, pos % MAX_MEASUREMENTS);
+            pos += 1;
+            match result{
+                Some(avg) => new_avg = avg,
+                None => continue,
+            }
+            if previous_avg < new_avg { // increased depth
+                count_increased_depth += 1;
+            }
+            previous_avg = new_avg;
+
+        }
+        println!("the depth increased: {} times", count_increased_depth);
+        Ok(())
+    }
+
+
+    #[derive(Debug)]
+pub struct SlideFilter {
+        slide_window: HashMap<u32, i32>,
+        measurement: Option<i32>, 
+    }
+    impl SlideFilter{
+        pub fn new() -> SlideFilter {
+            let map: HashMap<u32, i32> = HashMap::with_capacity(NUMBER_OF_MEASUREMENTS);
+            let measure:Option<i32> = None;
+            // default slide filter 
+            SlideFilter{
+                slide_window: map,
+                measurement: measure,
+            }
+        }
+        pub fn calculate_avg(&mut self, sample: i32, position: u32) -> Option<i32>{
+            let old_value = self.slide_window.insert(position, sample);
+            let mut sum = 0;
+
+            match self.measurement{
+                // filter is working, calculate most recent sum on window(updating only 1 value)
+                Some(avg) => sum = avg - old_value.unwrap() + sample,  
+                None => {
+                    if self.slide_window.len() != NUMBER_OF_MEASUREMENTS { // too few samples
+                        return None;
+                    } else { // calculate average for the first time
+                        for val in self.slide_window.values() {
+                            sum += val;
+                        }
+                    }
+                },
+            }
+            self.measurement = Some(sum);
+            self.measurement
+        }
+        pub fn remove_sample(&mut self, position: u32) {
+            self.slide_window.remove_entry(&position);
+            self.measurement = None;
+        }
+    }
 }
 
 
--- Projects/01_AdventCode2021/advent_of_code/src/main.rs
+++ Projects/01_AdventCode2021/advent_of_code/src/main.rs
@@ -1,6 +1,10 @@
 use advent_of_code::d01_sonar_sweep;
 use std::time::{ Instant};
 fn main() {
+  day_01();
+}
+
+pub fn day_01() {
     let mut start = Instant::now();
     println!(
         "status sonar: {:?}",
@@ -23,6 +27,15 @@ fn main() {
     println!(
         "status sonar functional2: {:?}",
         d01_sonar_sweep::sonar_sweep::depth_increase_functional2()
+    );
+    duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
+
+    start = Instant::now();
+    println!(
+        "status sonar functional2 slide window: {:?}",
+        d01_sonar_sweep::sonar_sweep::dept_increase_slide_filter()
     );
     duration = start.elapsed();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
