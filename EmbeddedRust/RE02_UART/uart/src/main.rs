d097b1bd3c85e183d282d2c7fff08d4b4354a801
Killerpug <etapia.clr@outlook.com>
2023-05-21 21:09:09 -0500

    uart exercise

--- EmbeddedRust/RE02_UART/uart/src/main.rs
+++ EmbeddedRust/RE02_UART/uart/src/main.rs
@@ -1,36 +1,48 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use log::*;
-use esp_idf_hal::delay::BLOCK;
-use esp_idf_hal::gpio;
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::uart::*;
+use esp_idf_hal::delay::BLOCK;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Hello, world!");
     let peripherals = Peripherals::take().unwrap();
-    let tx = peripherals.pins.gpio??;
-    let rx = peripherals.pins.gpio??;
+    let mut led = PinDriver::output(peripherals.pins.gpio12).unwrap();
+    let mut modem_pwrkey = PinDriver::output(peripherals.pins.gpio4).unwrap();
+    let mut modem_flight = PinDriver::output(peripherals.pins.gpio25).unwrap();
 
-    println!("Starting UART loopback test");
-    let config = config::Config::new().baudrate(Hertz(115_200));
-    let uart = UartDriver::new(
+    
+    // turn on gps module
+    modem_pwrkey.set_high().unwrap();
+    FreeRtos::delay_ms(1000);
+    modem_pwrkey.set_low().unwrap();
+    modem_flight.set_high().unwrap();
+
+    let tx = peripherals.pins.gpio1;
+    let rx = peripherals.pins.gpio2;
+
+    let config = config::Config::default().baudrate(Hertz(115_200));
+    let mut uart = UartDriver::new(
         peripherals.uart1,
         tx,
         rx,
-        None,
-        None,
+        Option::<Gpio0>::None,
+        Option::<Gpio1>::None,
         &config,
     )
     .unwrap();
-
+    println!("setup initialized");
+    led.set_high().unwrap();
     loop {
+        FreeRtos::delay_ms(1000);
+        led.set_low().unwrap();        
+        FreeRtos::delay_ms(1000);
+        led.set_high().unwrap();
         uart.write(&[0xaa]).unwrap();
 
         let mut buf = [0_u8; 1];
